2f5e0e389feb9a0e0598419076e4bad0cc246c7c
Felipe Rosa <flp.far@gmail.com>
2020-04-10 13:37:43 -0300

    Refactor method call and specs in scraper_spec

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,23 +1,19 @@
 require_relative '../lib/scraper.rb'
 
 describe Scraper do
-  let(:scraped_info) { Scraper.scrap }
+  before(:all) { @scraped_info = Scraper.scrap(2) }
 
   describe '.scrap' do
-    it 'returns an array with 250 items' do
-      scraped_info.should have(250).items
-    end
-
     it 'returns an array of hashes which all contain a key :title' do
-      expect(scraped_info).to all(have_key(:title))
+      expect(@scraped_info).to all(have_key(:title))
     end
 
     it 'returns an array of hashes which all contain a key :tag' do
-      expect(scraped_info).to all(have_key(:tag))
+      expect(@scraped_info).to all(have_key(:tag))
     end
 
     it 'returns an array of hashes which all contain a key :author' do
-      expect(scraped_info).to all(have_key(:author))
+      expect(@scraped_info).to all(have_key(:author))
     end
   end
 end
