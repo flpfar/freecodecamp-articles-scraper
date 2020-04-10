205d8413dc0c9c976c7a8d1b5f894c98b600e697
Felipe Rosa <flp.far@gmail.com>
2020-04-10 14:01:03 -0300

    Add new spec for Scraper

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -4,6 +4,10 @@ describe Scraper do
   before(:all) { @scraped_info = Scraper.scrap(2) }
 
   describe '.scrap' do
+    it 'returns an array with 50 items' do
+      expect(@scraped_info.size).to be(50)
+    end
+
     it 'returns an array of hashes which all contain a key :title' do
       expect(@scraped_info).to all(have_key(:title))
     end
