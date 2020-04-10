f329e22df25d3eab117b9c56fec4dbda4976ef4b
Felipe Rosa <flp.far@gmail.com>
2020-04-10 17:33:34 -0300

    Add filter_articles method to Filter

--- lib/filter.rb
+++ lib/filter.rb
@@ -4,6 +4,15 @@ class Filter
 
     def filter_authors(articles, number_of_articles); end
 
-    def filter_articles(articles, number_of_articles); end
+    def filter_articles(articles, number_of_articles = nil)
+      number_of_articles = articles.size if number_of_articles.nil?
+      filtered = []
+      index = 0
+      number_of_articles.times do
+        filtered.push(articles[index][:title])
+        index += 1
+      end
+      filtered
+    end
   end
 end
