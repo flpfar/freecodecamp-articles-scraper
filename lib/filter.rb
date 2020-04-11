3ef0b1f6d865d924c80efac0844bc6c202e99419
Felipe Rosa <flp.far@gmail.com>
2020-04-11 12:14:20 -0300

    Add .filter_authors to Filter class

--- lib/filter.rb
+++ lib/filter.rb
@@ -1,8 +1,28 @@
 class Filter
   class << self
-    def filter_tags(articles, number_of_articles); end
+    def filter_tags(articles, number_of_tags = nil)
+      return [] if articles.size.zero?
 
-    def filter_authors(articles, number_of_articles); end
+      tags = Hash.new(0)
+      articles.each do |article|
+        tags[article[:tag].to_sym] += 1
+      end
+      number_of_tags = tags.size if number_of_tags.nil? || number_of_tags > tags.size
+      Hash[tags.sort_by { |_key, value| -value }[0..number_of_tags - 1]]
+    end
+
+    def filter_authors(articles, number_of_authors = nil)
+      return [] if articles.size.zero?
+
+      authors = Hash.new { |hash, key| hash[key] = { profile: '', count: 0 } }
+      articles.each do |article|
+        author_name = article[:author][:name].to_sym
+        authors[author_name][:count] += 1
+        authors[author_name][:profile] = article[:author][:profile]
+      end
+      number_of_authors = articles.size if number_of_authors.nil? || number_of_authors > articles.size
+      test = Hash[authors.sort_by { |_key, value| -value[:count] }[0..number_of_authors - 1]]
+    end
 
     def filter_articles(articles, number_of_articles = nil)
       return [] if articles.size.zero?
